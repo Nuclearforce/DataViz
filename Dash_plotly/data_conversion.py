078685a7b44d0d9fb03f15e864c9dd3e3e37d085
Christiaan Brits <71432207+Nuclearforce@users.noreply.github.com>
2022-12-12 14:40:02 +0200

    Delete Dash_plotly directory

--- Dash_plotly/data_conversion.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-import dask.dataframe as dd
-ddf = dd.read_csv('../data/crypto_dataset/crypto-markets.csv', assume_missing=True)
-ddf.to_parquet('../data/crypto_dataset/crypto-markets.parquet')
